65b5015ed1e4ed9fef744edd73876afffdcf4386
Kirill Tarasenko <Kirill.Tarasenko@broit.io>
2025-12-19 12:42:06 +0300

    fix: move providers to separate file from main

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,3 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
-  zone = var.zone
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  service_account_key_file = var.path_to_authorized_key
-}
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = var.datadog_api_url
-}
